41bc6d88b1a6b188d812ddc8154cb026ae0ccb47
Jake Lane <lanejake13@gmail.com>
2020-10-27 12:42:10 -0400

    finished project

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,24 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            isOdd(5);
+        }
+
+        public static bool isOdd(int num)
+        {
+            Console.WriteLine("Enter number");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+                return true;
+            }
         }
     }
 }
